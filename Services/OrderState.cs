4799d907cb237357fe9a41134ab3b2dd23435737
trantuvan <trantuvan.kan@gmail.com>
2023-11-25 16:21:26 +0700

    Add AppState pattern to manage Size Order

---
+++ Services/OrderState.cs
@@ -0,0 +1,34 @@
+﻿namespace BlazingPizza.Services;
+
+public class OrderState
+{
+    public bool ShowingConfigureDialog { get; set; }
+    public Pizza ConfiguringPizza { get; private set; }
+    public Order Order { get; private set; } = new Order();
+
+    public void ShowingConfigurePizzaDialog(PizzaSpecial special)
+    {
+        ConfiguringPizza = new Pizza()
+        {
+            Special = special,
+            SpecialId = special.Id,
+            Size = Pizza.DefaultSize,
+            Toppings = new List<PizzaTopping>()
+        };
+
+        ShowingConfigureDialog = true;
+    }
+
+    public void CancelConfiguringPizzaDialog()
+    {
+        ConfiguringPizza = default;
+        ShowingConfigureDialog = false;
+    }
+
+    public void ConfirmConfiguringPizzaDialog()
+    {
+        Order.Pizzas.Add(ConfiguringPizza);
+        ConfiguringPizza = default;
+        ShowingConfigureDialog = false;
+    }
+}
