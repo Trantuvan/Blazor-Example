adbc099cb2a4fa51d98c6ba53cfdcd57d2f56124
trantuvan <trantuvan.kan@gmail.com>
2023-11-25 11:47:07 +0700

    1st commit

---
+++ Data/PizzaStoreContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza;
+
+public class PizzaStoreContext : DbContext
+{
+    public DbSet<PizzaSpecial> Specials { get; set; }
+    public PizzaStoreContext(DbContextOptions options) : base(options) { }
+}
