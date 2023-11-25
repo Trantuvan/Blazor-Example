845e4fa94b05a9144b7b8e88c0169a317a51b6e4
trantuvan <trantuvan.kan@gmail.com>
2023-11-25 12:04:45 +0700

    register HttpClient & Sqlite

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using BlazingPizza.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
+builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
 
 var app = builder.Build();
 
