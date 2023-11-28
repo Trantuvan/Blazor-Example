3ee4d8558369fd4e1254c7571f9f00ded7e0bd3e
trantuvan <trantuvan.kan@gmail.com>
2023-11-28 13:11:35 +0700

    Add More Tables & ManyToMany Pizza|Topping
    Summnary: Configure Sqlite with Ef core
    + Add tables" Orders | Pizzas | Toppings
    + Configure M:N Toppings | Pizzas

--- Data/PizzaStoreContext.cs
+++ Data/PizzaStoreContext.cs
@@ -5,5 +5,25 @@ namespace BlazingPizza.Data;
 public class PizzaStoreContext : DbContext
 {
     public DbSet<PizzaSpecial> Specials { get; set; }
+    public DbSet<Pizza> Pizzas { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Topping> Toppings { get; set; }
     public PizzaStoreContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        //* configure many to many relations 
+        modelBuilder.Entity<PizzaTopping>()
+            .HasKey(pst => new { pst.PizzaId, pst.ToppingId });
+
+        modelBuilder.Entity<PizzaTopping>()
+            .HasOne<Pizza>()
+            .WithMany(pst => pst.Toppings);
+
+        modelBuilder.Entity<PizzaTopping>()
+            .HasOne(pst => pst.Topping)
+            .WithMany();
+    }
 }
